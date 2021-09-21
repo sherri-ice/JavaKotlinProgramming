e6ca11541479c2262743030e5fb38d5094e08630
sherri.ice <sherri.ice7@gmail.com>
2021-09-21 20:56:28 +0300

    welcome message added

--- lab-01-expr-calc/src/by/sherriice/calc/Main.java
+++ lab-01-expr-calc/src/by/sherriice/calc/Main.java
@@ -4,18 +4,37 @@ import by.sherriice.calc.expressions.*;
 import by.sherriice.calc.test.ParserTest;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws ExpressionParseException {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Parser parser = new ParserImpl();
+        System.out.println("Welcome to the calculator :) \n" +
+                "Supported operations: +, -, *, /\n" +
+                "Variables are also supported");
 
-        var expr = parser.parseExpression(in.nextLine());
-        System.out.println(expr.accept(DebugRepresentationExpressionVisitor.INSTANCE));
-        Map<Character, Double> variables= (Map<Character, Double>) expr.accept(VariableInputVisitor.INSTANCE);
-        System.out.println(expr.accept(new ComputeExpressionVisitor(variables)));
-        System.out.println(expr.accept(TreeDepthVisitor.INSTANCE));
+        while (true) {
+            System.out.println("Enter your expression or 'E' if you finished:");
+            String input = in.nextLine();
+            if (Objects.equals(input, "E")) {
+                break;
+            }
+            Expression expr = null;
+            try {
+                expr = parser.parseExpression(input);
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+
+            }
+            if (expr != null) {
+                System.out.println("Your expression: " + expr.accept(DebugRepresentationExpressionVisitor.INSTANCE));
+                Map<Character, Double> variables = (Map<Character, Double>) expr.accept(VariableInputVisitor.INSTANCE);
+                System.out.println("Depth: " + expr.accept(TreeDepthVisitor.INSTANCE));
+                System.out.println("Result: " + expr.accept(new ComputeExpressionVisitor(variables)));
+            }
+        }
 
     }
 }
